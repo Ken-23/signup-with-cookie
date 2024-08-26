ba2de62da074983a459c875fdae11f543b287635
Ken 1 <ken.1@insurancedekho.com>
2024-08-26 15:58:58 +0530

    Comment added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (_req, res) {
-  res.send("Hello Express!!");
+  res.send("Hello Express!! ahjkafh");
 });
 
 module.exports = router;
